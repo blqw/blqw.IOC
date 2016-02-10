0c97b11ba39c00cce36060cc67050390a95e6012
blqw <fttl_398@126.com>
2016-02-11 03:09:30 +0800

    框架定型

---
+++ blqw.IOC/Component.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blqw.IOC
+{
+    [Export("Component")]
+    class Component
+    {
+        /// <summary> 获取动态类型
+        /// </summary>
+        [Import("GetDynamic")]
+        public static readonly Func<object, dynamic> GetDynamic = o => {
+            throw new NotImplementedException("需要Top.Convert3插件");
+        };
+
+        /// <summary> 序列化
+        /// </summary>
+        [Import("SerializeToBytes")]
+        public static readonly Func<object, byte[]> Serialize = o => {
+            throw new NotImplementedException("需要Top.Serializable插件");
+        };
+
+
+        /// <summary> 反序列化
+        /// </summary>
+        [Import("DeserializeFormBytes")]
+        public static readonly Func<byte[], object> Deserialize = o => {
+            throw new NotImplementedException("需要Top.Serializable插件");
+        };
+
+        /// <summary> 获取转换器
+        /// </summary>
+        [Import()]
+        public static readonly IFormatterConverter Converter = null;
+    }
+}
