4329af8a596878d4f69b76ea2bb32b7d47c04020
blqw <blqw@live.com>
2016-07-27 16:46:14 +0800

    111

---
+++ blqw.IOC/ComponentServices.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blqw.IOC
+{
+    /// <summary>
+    /// 组件服务
+    /// </summary>
+    public static class ComponentServices
+    {
+        static ComponentServices()
+        {
+            MEF.Import(typeof(ComponentServices));
+        }
+
+
+        /// <summary> 
+        /// 获取默认值
+        /// </summary>
+        [Import("GetDefaultValue")]
+        public static readonly Func<Type, object> GetDefaultValue = t => t == null || t.IsValueType == false || t.IsGenericTypeDefinition || Nullable.GetUnderlyingType(t) != null ? null : Activator.CreateInstance(t);
+        
+        /// <summary> 
+        /// 获取转换器
+        /// </summary>
+        [Import("Convert3")]
+        public static readonly IFormatterConverter Converter = new DefalutComponent.Converter();
+
+        [Import("JsonConverter")]
+        public static readonly IFormatterConverter Json;
+
+        [Import("XmlConverter")]
+        public static readonly IFormatterConverter Xml;
+
+        [Import("BinaryConverter")]
+        public static readonly IFormatterConverter Binary;
+
+        /// <summary> 
+        /// 包装反射对象
+        /// </summary>
+        [Import("MemberInfoWrapper")]
+        public static readonly Func<MemberInfo, MemberInfo> WrapMamber = m => m;
+        
+        /// <summary> 
+        /// 用于将Json字符串转为实体对象的方法
+        /// </summary>
+        [Import("ToJsonObject")]
+        public readonly static Func<Type, string, object> ToJsonObject =
+            delegate (Type type, string json)
+            {
+                return Json.Convert(json, type);
+            };
+
+        /// <summary> 
+        /// 用于将Json字符串转为实体对象的方法
+        /// </summary>
+        [Import("ToJsonString")]
+        public readonly static Func<object, string> ToJsonString =
+            delegate (object obj)
+            {
+                return Json.ToString(obj);
+            };
+
+
+        /// <summary> 获取动态类型
+        /// </summary>
+        [Import("GetDynamic")]
+        public static readonly Func<object, dynamic> GetDynamic ;
+
+
+
+
+    }
+}
---
+++ blqw.IOC/DefalutComponent.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blqw.IOC
+{
+
+    class DefalutComponent
+    {
+        public static object Convert(object value, Type type, bool throwError)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            if (type == typeof(string))
+            {
+                return value.ToString();
+            }
+            var str = value as string;
+            if (str == null)
+            {
+                if (value == null)
+                {
+                    return null;
+                }
+                try
+                {
+                    return System.Convert.ChangeType(value, type);
+                }
+                catch
+                {
+                    if (throwError)
+                    {
+                        throw;
+                    }
+                    return null;
+                }
+            }
+            if (type == typeof(Guid))
+            {
+                Guid g;
+                if (Guid.TryParse(str, out g))
+                {
+                    return g;
+                }
+            }
+            else if (type == typeof(Uri))
+            {
+                Uri u;
+                if (Uri.TryCreate(str, UriKind.RelativeOrAbsolute, out u))
+                {
+                    return u;
+                }
+            }
+            else if (type == typeof(TimeSpan))
+            {
+                TimeSpan t;
+                if (TimeSpan.TryParse(str, out t))
+                {
+                    return t;
+                }
+            }
+            else if (type == typeof(Type))
+            {
+                return Type.GetType(str, false, true);
+            }
+            else
+            {
+                try
+                {
+                    return System.Convert.ChangeType(value, type);
+                }
+                catch
+                {
+                    if (throwError)
+                    {
+                        throw;
+                    }
+                    return null;
+                }
+            }
+            if (throwError)
+            {
+                throw new InvalidCastException($"字符串: {str} 转为类型:{ComponentServices.Converter?.ToString(type) ?? type?.ToString()}失败");
+            }
+            return null;
+
+        }
+
+        public class Converter : FormatterConverter, IFormatterConverter
+        {
+
+            #region IFormatterConverter 成员
+
+            object IFormatterConverter.Convert(object value, Type type)
+            {
+                return DefalutComponent.Convert(value, type, true);
+            }
+
+            #endregion
+        }
+
+    }
+}
