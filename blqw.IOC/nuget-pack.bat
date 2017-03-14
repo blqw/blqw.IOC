89cf095b55efd147a6e4572adbb79982d4537f89
周子鉴 <blqw@live.com>
2017-03-15 03:17:16 +0800

    更新nuget push脚本

--- blqw.IOC/nuget-pack.bat
+++ blqw.IOC/nuget-pack.bat
@@ -1,9 +1,9 @@
 for /r . %%a in (*.nupkg) do (
-	del %%a
+	del "%%a"
 )
 for /r . %%a in (*.nuspec) do (
-	nuget pack %%~dpna.csproj -build  -Prop Configuration=Release
+	nuget pack "%%~dpna.csproj" -build  -Prop Configuration=Release
 )
 for /r . %%a in (*.nupkg) do (
-	nuget push %%a -source https://www.nuget.org/api/v2/package -apikey %1%
+	nuget push "%%a" -source https://www.nuget.org/api/v2/package -apikey %1%
 )
